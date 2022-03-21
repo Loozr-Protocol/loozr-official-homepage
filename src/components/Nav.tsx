1185fb5adbb6117acdb8a7a0cee7b8b3b6f3d83d
Confidence Yobo <confiyobo@gmail.com>
2022-03-21 17:56:54 +0100

    Update top nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,55 +1,60 @@
 function Nav() {
   return (
-  <nav className="navbar navbar-expand-lg">
-    <div className="container">
-      <div id="block-avo-branding">
-        <a href="/" className="logo">
-          <img
-            className="logo-light"
-            src="/img/logo-light.png"
-            alt="Home"
-          />
-        </a>
-      </div>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="icon-bar"><i className="fas fa-bars"></i></span>
-      </button>
+    <nav className="navbar navbar-expand-lg">
+      <div className="container">
+        <div id="block-avo-branding">
+          <a href="/" className="logo">
+            <img className="logo-light" src="/img/logo-light.png" alt="Home" />
+          </a>
+        </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="icon-bar">
+            <i className="fas fa-bars"></i>
+          </span>
+        </button>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <a className="nav-link" href="https://loozr-1.gitbook.io/docs/"
-            >Documentation</a
-            >
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="https://medium.com/@officialloozr"
-            >Blog</a
-            >
-          </li>
-          <li className="nav-item">
-              <a className="nav-link" href="https://loozr-1.gitbook.io/docs/">Litepaper</a>
-          </li>
-          <li className="nav-item">
-            <a
-              className="nav-link"
-                href="https://discord.gg/mm5mV8PHpq" rel="noopener noreferrer"
-              target="_blank"
-            >Join Community</a
-            >
-          </li>
-        </ul>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="https://loozr-1.gitbook.io/docs/">
+                LiteDoc
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="https://medium.com/@officialloozr">
+                Blog
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="https://discord.gg/mm5mV8PHpq"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Join Telegram
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link nav-btn"
+                href="https://discord.gg/mm5mV8PHpq"
+              >
+                Join Whitelisting
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
   );
 }
 
