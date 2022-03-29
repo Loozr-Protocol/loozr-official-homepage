eebd6f7341f04e7238fb4c6281604c9923dc3d80
Confidence Yobo <confiyobo@gmail.com>
2022-03-29 13:43:17 +0100

    Minor text update

---
+++ src/containers/ArtistsEcosystem.tsx
@@ -0,0 +1,55 @@
+export default function ArtistEcosystem() {
+  const tbgStyle = {
+    bottom: "60px",
+    fontSize: "14vw",
+  };
+  const gridImages = [
+    <img src="/img/gallery/1.png" alt="" />,
+    <img src="/img/gallery/2.png" alt="" />,
+    <img src="/img/gallery/3.png" alt="" />,
+  ];
+  return (
+    <>
+      <div className="main-content">
+        <div>
+          <div
+            id="block-nvblockaboutindex"
+            className="about section-padding mt-30"
+          >
+            <div className="container">
+              <div className="sec-head custom-font text-center mb-10">
+                <h6 className="wow fadeIn" data-wow-delay=".5s">
+                  CREATOR ECOSYSTEM
+                </h6>
+                <h3 className="wow" data-splitting>
+                  TOP ARTISTES
+                </h3>
+                <span className="tbg">TOP ARTISTES</span>
+              </div>
+            </div>
+            <div className="clearfix"></div>
+            <div className="container">
+              <div className="col-md-6 mx-auto">
+                <p className="text-center">
+                  Here are some of the musicians and creators who have
+                  contributed to our early and ongoing development phase. Loozr
+                  is enabling independent digital economies to be built by
+                  artistes and their communities.
+                </p>
+              </div>
+            </div>
+            <div className="container d-flex align-items-around">
+              //
+              {/* {gridImages.map(image, i) => (
+<div className="col-6 col-sm4">
+
+                </div>
+              )}§ */}
+            </div>
+          </div>
+          <div className="clearfix"></div>
+        </div>
+      </div>
+    </>
+  );
+}
