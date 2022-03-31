66d168b179ee546e377ada2a08bdaf1f567168ec
Confidence Yobo <confiyobo@gmail.com>
2022-03-31 11:42:41 +0100

    Update service worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -64,7 +64,21 @@ registerRoute(
     plugins: [
       // Ensure that once this runtime cache reaches a maximum size the
       // least-recently used images are removed.
-      new ExpirationPlugin({ maxEntries: 50 }),
+      new ExpirationPlugin({ maxEntries: 50, maxAgeSeconds: 7200 }),
+    ],
+  })
+);
+
+registerRoute(
+  // Add in any other file extensions or routing criteria as needed.
+  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.js'),
+  // Customize this strategy as needed, e.g., by changing to CacheFirst.
+  new StaleWhileRevalidate({
+    cacheName: 'javascript',
+    plugins: [
+      // Ensure that once this runtime cache reaches a maximum size the
+      // least-recently used javascript are removed.
+      new ExpirationPlugin({ maxEntries: 50, maxAgeSeconds: 7200 }),
     ],
   })
 );
