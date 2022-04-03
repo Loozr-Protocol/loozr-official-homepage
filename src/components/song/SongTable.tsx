1a0a75898f1583693f106499407fa4160fa029c6
Confidence Yobo <confiyobo@gmail.com>
2022-04-03 20:33:35 +0100

    Make music details clickable from song table

--- src/components/song/SongTable.tsx
+++ src/components/song/SongTable.tsx
@@ -11,16 +11,16 @@ export default function SongTable({ id, songId, song }: SongTableProps) {
   return (
     <>
       <div className="row mt-40">
-        <div className="col-md-3">
-          <div className="d-flex">
+        <div className="col-md-4">
+          <Link className="d-flex" to={`/artists/${id}/songs/${songId}`}>
             <img src={song.photo} className="vxthumbnail" alt="" />
             <div className="vxdescription">
               <h4>{song.name}</h4>
               <p>{song.coinValue} LZR Coin Price</p>
             </div>
-          </div>
+          </Link>
         </div>
-        <div className="col-md-8 partial-offset-md-1 sm-mt10">
+        <div className="col-md-7 partial-offset-md-1 sm-mt10">
           <div className="d-flex align-items-center">
             <p className="vxcaption">{song.duration} mins</p>
             <img src="/img/icon-heart.png" className="ml-4 img-icon" alt="" />
@@ -37,7 +37,10 @@ export default function SongTable({ id, songId, song }: SongTableProps) {
                 className="dropdown-menu"
                 aria-labelledby="dropdownMenuButton"
               >
-                <Link className="dropdown-item" to={`/artists/${id}/songs/${songId}`}>
+                <Link
+                  className="dropdown-item"
+                  to={`/artists/${id}/songs/${songId}`}
+                >
                   View Song
                 </Link>
                 <Link className="dropdown-item" to="/">
