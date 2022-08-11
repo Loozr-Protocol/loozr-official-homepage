d905d43f3079de3d1313e34d2cdba2decde64b7f
Gifted <gkenny896@gmail.com>
2022-08-11 08:11:55 +0100

    Fix: mobile nav

--- src/containers/Artistes.tsx
+++ src/containers/Artistes.tsx
@@ -1,5 +1,109 @@
 import React from "react";
+import VerifiedBadge from "../assets/icons/verified_badge.svg";
+
 import Arlene from "../assets/img/artists/arlene.png";
+import Magnus from "../assets/img/artists/magnus.png";
+import Christian from "../assets/img/artists/christian.png";
+import Atlassian from "../assets/img/artists/atlasian.png";
+
+export const featured = [
+  {
+    name: "$ARLENE",
+    img: Arlene,
+    price: "2,474.14 LZR",
+    verified: true,
+  },
+  {
+    name: "$MAGNUS_",
+    img: Magnus,
+    price: "404.99 LZR",
+    verified: true,
+  },
+  {
+    name: "$CHRISTIAN",
+    img: Christian,
+    price: "35.93 LZR",
+    verified: false,
+  },
+  {
+    name: "$ATLAISIAN",
+    img: Atlassian,
+    price: "1,444.93 LZR",
+    verified: true,
+  },
+  {
+    name: "$CHRISTIAN",
+    img: Christian,
+    price: "35.93 LZR",
+    verified: false,
+  },
+  {
+    name: "$MAGNUS_",
+    img: Magnus,
+    price: "404.99 LZR",
+    verified: false,
+  },
+  {
+    name: "$CHRISTIAN",
+    img: Christian,
+    price: "35.93 LZR",
+    verified: true,
+  },
+  {
+    name: "$ARLENE",
+    img: Arlene,
+    price: "2,474.14 LZR",
+    verified: true,
+  },
+  {
+    name: "$MAGNUS_",
+    img: Magnus,
+    price: "404.99 LZR",
+    verified: true,
+  },
+  {
+    name: "$CHRISTIAN",
+    img: Christian,
+    price: "35.93 LZR",
+    verified: false,
+  },
+  {
+    name: "$ATLAISIAN",
+    img: Atlassian,
+    price: "1,444.93 LZR",
+    verified: true,
+  },
+  {
+    name: "$CHRISTIAN",
+    img: Christian,
+    price: "35.93 LZR",
+    verified: false,
+  },
+  {
+    name: "$ATLAISIAN",
+    img: Atlassian,
+    price: "1,444.93 LZR",
+    verified: false,
+  },
+  {
+    name: "$ARLENE",
+    img: Arlene,
+    price: "2,474.14 LZR",
+    verified: false,
+  },
+  {
+    name: "$MAGNUS_",
+    img: Magnus,
+    price: "404.99 LZR",
+    verified: true,
+  },
+  {
+    name: "$CHRISTIAN",
+    img: Christian,
+    price: "35.93 LZR",
+    verified: false,
+  },
+];
 
 const Artistes = () => {
   return (
@@ -8,29 +112,36 @@ const Artistes = () => {
         Artistes
       </p>
       <div className="grid grid-cols-3 md:grid-cols-4 gap-y-16">
-        {Array(20)
-          .fill(0)
-          .map((_, i) => (
-            <div
-              key={i}
-              className="flex flex-col items-center mr-4 min-w-full md:min-w-[170px]"
-            >
+        {featured.map((_, i) => (
+          <div
+            key={i}
+            className="flex flex-col items-center mr-4 min-w-full md:min-w-[140px]"
+          >
+            <div className="relative">
               <img
-                src={Arlene}
+                src={_.img}
                 alt=""
-                className="object-cover h-36 w-36 rounded-full border-[15px] border-dark-700 mb-[18px]"
+                className="object-cover h-24 w-24 md:h-32 md:w-32 rounded-full border-[15px] border-dark-700 mb-[18px]"
                 style={{
                   border: "10px solid #141922",
                 }}
               />
-              <p className="mb-1 font-normal md:font-bold text-base md:text-base text-white text-center">
-                $ARLENE
-              </p>
-              <p className="text-muted text-xs font-medium md:font-semibold text-center">
-                2,474.14 LZR
-              </p>
+              {_.verified && (
+                <img
+                  src={VerifiedBadge}
+                  alt=""
+                  className="absolute w-4 md:w-6 h-4 md:h-6 right-3 bottom-6"
+                />
+              )}
             </div>
-          ))}
+            <p className="mb-1 font-normal md:font-medium text-base md:text-base text-white text-center">
+              $ARLENE
+            </p>
+            <p className="text-muted text-xs font-light md:font-medium text-center">
+              2,474.14 LZR
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
