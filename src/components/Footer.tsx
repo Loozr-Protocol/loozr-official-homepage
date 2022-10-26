399534bcc5278abdef958cb17bd05e6e09799fc3
Confidence Yobo <confiyobo@gmail.com>
2022-10-26 18:05:50 +0100

    Change discord link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,28 +11,36 @@ export default function Footer() {
                   <div className="social">
                     <a
                       href="https://twitter.com/officialloozr"
-                      target="_blank" rel="noopener noreferrer"
+                      target="_blank"
+                      rel="noopener noreferrer"
                       className="icon"
-                    ><i className="fab fa-twitter"></i
-                    ></a>
+                    >
+                      <i className="fab fa-twitter"></i>
+                    </a>
                     <a
-                      href="https://discord.gg/mm5mV8PHpq"
-                      target="_blank" rel="noopener noreferrer"
+                      href="https://discord.gg/M7S7DbTbfu"
+                      target="_blank"
+                      rel="noopener noreferrer"
                       className="icon"
-                    ><img src="/img/discord.png" alt="discord"
-                      /></a>
+                    >
+                      <img src="/img/discord.png" alt="discord" />
+                    </a>
                     <a
                       href="https://t.me/+Zi7-M7wMW6A4MjE0"
-                      target="_blank" rel="noopener noreferrer"
+                      target="_blank"
+                      rel="noopener noreferrer"
                       className="icon"
-                    ><i className="fab fa-telegram-plane"></i
-                    ></a>
+                    >
+                      <i className="fab fa-telegram-plane"></i>
+                    </a>
                     <a
                       href="https://medium.com/@officialloozr"
-                      target="_blank" rel="noopener noreferrer"
+                      target="_blank"
+                      rel="noopener noreferrer"
                       className="icon"
-                    ><i className="fab fa-medium-m"></i
-                    ></a>
+                    >
+                      <i className="fab fa-medium-m"></i>
+                    </a>
                   </div>
                 </div>
               </div>
