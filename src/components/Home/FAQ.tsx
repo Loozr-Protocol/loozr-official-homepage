8766fe193bc3defa2e2c2edc812e589855e4dfc6
egileekada <egileoniso.ekada@gmail.com>
2023-02-10 17:48:00 +0100

    update on landing page

--- src/components/Home/FAQ.tsx
+++ src/components/Home/FAQ.tsx
@@ -26,15 +26,15 @@ export default function FAQ() {
             </div>
             <div className=' w-full flex mt-6 ' >
                 <div className=' w-full flex justify-center pr-10 ' >
-                    <p className=' max-w-[450px] text-white font-medium  ' >Haven’t found what you’re looking for?
+                    <p className=' max-w-[450px] text-white wow txt !text-[14.7px] !font-normal  ' >Haven’t found what you’re looking for?
                         Try emailing <span  id="gradenttext" >support@loozr.io</span> — we’re here to help.</p>
                 </div>
                 <div className=' w-full flex justify-center pl-10 text-white ' >
                     <div className=' w-[600px] ' >
                         <div className=' w-full border-b py-4 border-[#536079] ' > 
-                            <div className=' flex items-center justify-between ' >
+                            <div role="button" onClick={()=> clickHandler(0)}  className=' flex items-center justify-between ' >
                                 <p className=' text-xl font-bold ' >What is Loozr? </p>
-                                <svg onClick={()=> clickHandler(0)} className={tab === 0 ? " -rotate-180 ": ""} role='button' width="17" height="9" viewBox="0 0 17 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <svg className={tab === 0 ? " -rotate-180 ": ""} role='button' width="17" height="9" viewBox="0 0 17 9" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M0.232592 0.232592C0.514521 -0.0493376 0.955694 -0.0749676 1.26658 0.155702L1.35564 0.232592L8.20588 7.08247L15.0561 0.232592C15.3381 -0.0493376 15.7792 -0.0749676 16.0901 0.155702L16.1792 0.232592C16.4611 0.514521 16.4867 0.955694 16.2561 1.26658L16.1792 1.35564L8.76741 8.76741C8.48548 9.04934 8.04431 9.07497 7.73342 8.8443L7.64436 8.76741L0.232592 1.35564C-0.0775306 1.04552 -0.0775306 0.542714 0.232592 0.232592Z" fill="white"/>
                                 </svg>
                             </div>
@@ -45,22 +45,22 @@ export default function FAQ() {
                             )}
                         </div>
                         <div className=' w-full border-b py-4 border-[#536079] ' > 
-                            <div className=' flex items-center justify-between ' >
+                            <div role="button" onClick={()=> clickHandler(1)} className=' flex items-center justify-between ' >
                                 <p className=' text-xl font-bold ' >How do I get my LNS?</p>
-                                <svg onClick={()=> clickHandler(1)} className={tab === 1 ? " -rotate-180 ": ""} role='button' width="17" height="9" viewBox="0 0 17 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <svg className={tab === 1 ? " -rotate-180 ": ""} role='button' width="17" height="9" viewBox="0 0 17 9" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M0.232592 0.232592C0.514521 -0.0493376 0.955694 -0.0749676 1.26658 0.155702L1.35564 0.232592L8.20588 7.08247L15.0561 0.232592C15.3381 -0.0493376 15.7792 -0.0749676 16.0901 0.155702L16.1792 0.232592C16.4611 0.514521 16.4867 0.955694 16.2561 1.26658L16.1792 1.35564L8.76741 8.76741C8.48548 9.04934 8.04431 9.07497 7.73342 8.8443L7.64436 8.76741L0.232592 1.35564C-0.0775306 1.04552 -0.0775306 0.542714 0.232592 0.232592Z" fill="white"/>
                                 </svg>
                             </div>
                             {tab === 1 && ( 
                                 <motion.p
                                 initial={{ y: -100, opacity: 0 }}
-                                animate={{ y: 0, opacity: 1 }} className=' mt-3 ' >Token-powered creator economy platform that allows anyone to invest in and profit from music and content creators' success per second.</motion.p>
+                                animate={{ y: 0, opacity: 1 }} className=' mt-3 wow txt !text-[14.7px] !font-normal ' >Token-powered creator economy platform that allows anyone to invest in and profit from music and content creators' success per second.</motion.p>
                             )}
                         </div>
                         <div className=' w-full border-b py-4 border-[#536079] ' > 
-                            <div className=' flex items-center justify-between ' >
+                            <div role="button" onClick={()=> clickHandler(2)} className=' flex items-center justify-between ' >
                                 <p className=' text-xl font-bold ' >How do I become an artists? </p>
-                                <svg onClick={()=> clickHandler(2)} className={tab === 2 ? " -rotate-180 ": ""} role='button' width="17" height="9" viewBox="0 0 17 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <svg className={tab === 2 ? " -rotate-180 ": ""} role='button' width="17" height="9" viewBox="0 0 17 9" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M0.232592 0.232592C0.514521 -0.0493376 0.955694 -0.0749676 1.26658 0.155702L1.35564 0.232592L8.20588 7.08247L15.0561 0.232592C15.3381 -0.0493376 15.7792 -0.0749676 16.0901 0.155702L16.1792 0.232592C16.4611 0.514521 16.4867 0.955694 16.2561 1.26658L16.1792 1.35564L8.76741 8.76741C8.48548 9.04934 8.04431 9.07497 7.73342 8.8443L7.64436 8.76741L0.232592 1.35564C-0.0775306 1.04552 -0.0775306 0.542714 0.232592 0.232592Z" fill="white"/>
                                 </svg>
                             </div>
@@ -71,9 +71,9 @@ export default function FAQ() {
                             )}
                         </div>
                         <div className=' w-full border-b py-4 border-[#536079] ' > 
-                            <div className=' flex items-center justify-between ' >
+                            <div role="button" onClick={()=> clickHandler(3)} className=' flex items-center justify-between ' >
                                 <p className=' text-xl font-bold ' >How to invest in an artist? </p>
-                                <svg onClick={()=> clickHandler(3)} className={tab === 3 ? " -rotate-180 ": ""} role='button' width="17" height="9" viewBox="0 0 17 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <svg className={tab === 3 ? " -rotate-180 ": ""} role='button' width="17" height="9" viewBox="0 0 17 9" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M0.232592 0.232592C0.514521 -0.0493376 0.955694 -0.0749676 1.26658 0.155702L1.35564 0.232592L8.20588 7.08247L15.0561 0.232592C15.3381 -0.0493376 15.7792 -0.0749676 16.0901 0.155702L16.1792 0.232592C16.4611 0.514521 16.4867 0.955694 16.2561 1.26658L16.1792 1.35564L8.76741 8.76741C8.48548 9.04934 8.04431 9.07497 7.73342 8.8443L7.64436 8.76741L0.232592 1.35564C-0.0775306 1.04552 -0.0775306 0.542714 0.232592 0.232592Z" fill="white"/>
                                 </svg>
                             </div>
@@ -84,9 +84,9 @@ export default function FAQ() {
                             )}
                         </div>
                         <div className=' w-full border-b py-4 border-[#536079] ' > 
-                            <div className=' flex items-center justify-between ' >
+                            <div role="button" onClick={()=> clickHandler(4)} className=' flex items-center justify-between ' >
                                 <p className=' text-xl font-bold ' >Is Loozr limited to only musicians? </p>
-                                <svg onClick={()=> clickHandler(4)} className={tab === 4 ? " -rotate-180 ": ""} role='button' width="17" height="9" viewBox="0 0 17 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <svg className={tab === 4 ? " -rotate-180 ": ""} role='button' width="17" height="9" viewBox="0 0 17 9" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M0.232592 0.232592C0.514521 -0.0493376 0.955694 -0.0749676 1.26658 0.155702L1.35564 0.232592L8.20588 7.08247L15.0561 0.232592C15.3381 -0.0493376 15.7792 -0.0749676 16.0901 0.155702L16.1792 0.232592C16.4611 0.514521 16.4867 0.955694 16.2561 1.26658L16.1792 1.35564L8.76741 8.76741C8.48548 9.04934 8.04431 9.07497 7.73342 8.8443L7.64436 8.76741L0.232592 1.35564C-0.0775306 1.04552 -0.0775306 0.542714 0.232592 0.232592Z" fill="white"/>
                                 </svg>
                             </div>
--- src/components/Home/FeatureSection.tsx
+++ src/components/Home/FeatureSection.tsx
@@ -5,50 +5,50 @@ export default function FeatureSection() {
         <> 
             <div className=' w-full bg-[#8369F4] !text-[#0C0F15] px-20 py-8 lg:py-0 lg:pt-20 relative ' > 
                 <div className=" ">
-                    <p className="wow fadeIn !text-[#0C0F15] !font-bold !leading-[1.1] !text-4xl " data-wow-delay=".5s">
+                    <p className="wow fadeIn !text-[#0C0F15] txt !font-extrabold !leading-[1.1] !text-4xl " data-wow-delay=".5s">
                         FEATURES 
                     </p>
-                <p className="  text-[15px] !font-medium text-[#0C0F15] lg:max-w-3xl" >Loozr empowers creators through various services, which they can utilize via the following features:</p>
+                <p className="wow txt !text-[14.6px] !leading-normal  mt-4 !font-normal text-[#0C0F15] lg:max-w-2xl" >Loozr empowers creators through various services, which they can utilize via the following features:</p>
                     {/* <h3 className="wow text-[#53607940]" data-splitting>
                         Features!
                     </h3> */}
                 </div>
-                <div className=' w-full  grid grid-cols-1 lg:grid-cols-3 gap-8 mt-8 mb-20 ' >
+                <div className=' w-full  grid grid-cols-1 lg:grid-cols-3 gap-9 mt-8 mb-20 ' >
                     <div className=' w-full ' > 
-                        <p className=' font-bold mt-3 text-2xl !text-[#0C0F15] ' >Instant Tokenization & Trading</p>
-                        <p className=' font-medium mt-2 !leading-normal !text-[#0C0F15] ' >Investible music for all. </p>
+                        <p className=' txt font-bold mt-3 text-[20px] !text-[#0C0F15] leading-normal ' >Instant Tokenization & Trading</p>
+                        <p className='wow txt !text-[14.7px] !font-normal mt-1  !text-[#0C0F15]  leading-normal ' >Investible music for all. </p>
                     </div>
                     <div className=' w-full ' > 
-                        <p className=' font-bold mt-3 text-2xl !text-[#0C0F15] ' >Community-driven ownership </p>
-                        <p className=' font-medium mt-2 !leading-normal !text-[#0C0F15] ' >Take control of your content, launch your own independent economy, and succeed together. </p>
+                        <p className=' txt font-bold mt-3 text-[20px] !text-[#0C0F15] leading-normal ' >Community-driven ownership </p>
+                        <p className='wow txt !text-[14.7px] !font-normal mt-1  !text-[#0C0F15]  leading-normal ' >Take control of your content, launch your own independent economy, and succeed together. </p>
                     </div>
                     <div className=' w-full ' > 
-                        <p className=' font-bold mt-3 text-2xl !text-[#0C0F15] ' >Transparency & smart contracts</p>
-                        <p className=' font-medium mt-2 !leading-normal !text-[#0C0F15] ' >Decentralizing finance, contracts, and establishing a completely open royalty and streaming accounting system.</p>
+                        <p className=' txt font-bold mt-3 text-[20px] !text-[#0C0F15] leading-normal ' >Transparency & smart contracts</p>
+                        <p className='wow txt !text-[14.7px] !font-normal mt-1  !text-[#0C0F15]  leading-normal ' >Decentralizing finance, contracts, and establishing a completely open royalty and streaming accounting system.</p>
                     </div>
                     <div className=' w-full ' > 
-                        <p className=' font-bold mt-3 text-2xl !text-[#0C0F15] ' >Secure and scalable </p>
-                        <p className=' font-medium mt-2 !leading-normal !text-[#0C0F15] ' >Transactions are carried out using the NEAR Protocol, a reliable and secure L1 with sharding support.</p>
+                        <p className=' txt font-bold mt-3 text-[20px] !text-[#0C0F15] leading-normal ' >Secure and scalable </p>
+                        <p className='wow txt !text-[14.7px] !font-normal mt-1  !text-[#0C0F15]  leading-normal ' >Transactions are carried out using the NEAR Protocol, a reliable and secure L1 with sharding support.</p>
                     </div>
                     <div className=' w-full ' > 
-                        <p className=' font-bold mt-3 text-2xl !text-[#0C0F15] ' >Launchpad for creators </p>
-                        <p className=' font-medium mt-2 !leading-normal !text-[#0C0F15] ' >Web3-native crowdinvesting platform that allows you to start, fund, and trade the Next Big Talent.</p>
+                        <p className=' txt font-bold mt-3 text-[20px] !text-[#0C0F15] leading-normal ' >Launchpad for creators </p>
+                        <p className='wow txt !text-[14.7px] !font-normal mt-1  !text-[#0C0F15]  leading-normal ' >Web3-native crowdinvesting platform that allows you to start, fund, and trade the Next Big Talent.</p>
                     </div>
                     <div className=' w-full ' > 
-                        <p className=' font-bold mt-3 text-2xl !text-[#0C0F15] ' >Lightning fast and self-sustainable</p>
-                        <p className=' font-medium mt-2 !leading-normal !text-[#0C0F15] ' >Instant payouts, better monetization, and directly rewarding artists, fans, and creators.</p>
+                        <p className=' txt font-bold mt-3 text-[20px] !text-[#0C0F15] leading-normal ' >Lightning fast and self-sustainable</p>
+                        <p className='wow txt !text-[14.7px] !font-normal mt-1  !text-[#0C0F15]  leading-normal ' >Instant payouts, better monetization, and directly rewarding artists, fans, and creators.</p>
                     </div>
                     <div className=' w-full ' > 
-                        <p className=' font-bold mt-3 text-2xl !text-[#0C0F15] ' >Frictionless user experience </p>
-                        <p className=' font-medium mt-2 !leading-normal !text-[#0C0F15] ' >Web2 frontend feel, Web3 backend integration — No knowledge of crypto needed.</p>
+                        <p className=' txt font-bold mt-3 text-[20px] !text-[#0C0F15] leading-normal ' >Frictionless user experience </p>
+                        <p className='wow txt !text-[14.7px] !font-normal mt-1  !text-[#0C0F15]  leading-normal ' >Web2 frontend feel, Web3 backend integration — No knowledge of crypto needed.</p>
                     </div>
                     <div className=' w-full ' > 
-                        <p className=' font-bold mt-3 text-2xl !text-[#0C0F15] ' >Unlimited music distribution</p>
-                        <p className=' font-medium mt-2 !leading-normal !text-[#0C0F15] ' >Distribute with one click to over 220+ stores globally, including Tiktok & Youtube. Decentralize your finances and stats on-chain. Keep 100% of your song's royalties and rights.</p>
+                        <p className=' txt font-bold mt-3 text-[20px] !text-[#0C0F15] leading-normal ' >Unlimited music distribution</p>
+                        <p className='wow txt !text-[14.7px] !font-normal mt-1  !text-[#0C0F15]  leading-normal ' >Distribute with one click to over 220+ stores globally, including Tiktok & Youtube. Decentralize your finances and stats on-chain. Keep 100% of your song's royalties and rights.</p>
                     </div>
                     <div className=' w-full ' > 
-                        <p className=' font-bold mt-3 text-2xl !text-[#0C0F15] ' >Mining via music streaming</p>
-                        <p className=' font-medium mt-2 !leading-normal !text-[#0C0F15] ' >Loozr's <span className=' text-white ' >#Listen2Earn</span> model allows music lovers to earn tokens whenever their favourite music is played.</p>
+                        <p className=' txt font-bold mt-3 text-[20px] !text-[#0C0F15] leading-normal ' >Mining via music streaming</p>
+                        <p className='wow txt !text-[14.7px] !font-normal mt-1  !text-[#0C0F15]  leading-normal ' >Loozr's <span className=' text-white ' >#Listen2Earn</span> model allows music lovers to earn tokens whenever their favourite music is played.</p>
                     </div>
                 </div> 
                 <div className=' w-full h-[400px] ' > 
