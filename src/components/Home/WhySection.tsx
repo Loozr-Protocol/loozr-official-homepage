faf2f40b993241704a43f376d24a9a3afbe5747c
egileekada <egileoniso.ekada@gmail.com>
2023-02-18 13:42:37 +0100

    update on landing page

--- src/components/Home/WhySection.tsx
+++ src/components/Home/WhySection.tsx
@@ -37,11 +37,7 @@ export default function WhySection() {
 
     return isIntersecting;
   }  
-  console.log(isInViewport1);
-  
-
-  const callback = function(index: any){   
-  }  
+  console.log(isInViewport1); 
 
   let slides = [
     <div className=" w-full flex !bg-[#0c0f15] flex-col items-center" >  
@@ -93,7 +89,7 @@ export default function WhySection() {
       <div className=" py-8 w-full flex flex-col items-center " >  
         <div  className=" relative max-w-3xl z-50 " > 
           <p  id="gradenttext"  className=" font-bold text-2xl text-left  ">For Music Creators</p>  
-          <div className=" py-2 mt-6 px-1 " >
+          <div className=" w-full py-2 mt-6 px-1 " >
             <div className=" flex text-white font-medium " >
               <div className=" w-fit " > 
                 <div className=" w-[4px] h-[4px] mt-[12px]  bg-white rounded-full mr-2 " />
@@ -114,20 +110,23 @@ export default function WhySection() {
   return (
     <>  
       <div className={ " w-full !bg-[#0c0f15] pt-24 flex flex-col  h-[100vh] "} >
-        <div className="container mb-12 ">
-          <div className="sec-head custom-font text-center">
+        <div className="w-full flex items-center relative lg:px-24 mb-12 ">
+          <div className="sec-head custom-font  w-full text-left">
             <h6 id="gradenttext" className=" leading-[1.8] relative  !font-medium !text-[15px] " data-wow-delay=".5s">
               WHO ARE WE
             </h6>
-            <h3 className=" !font-bold !text-white mt-8 !leading-[1.1] !text-4xl" data-splitting>
-              BUILDING FOR?
-            </h3>
-            <span className="tbg">BUILDING FOR?</span>
+            <p className=" !font-bold txt !text-white text-left mt-8 !leading-[1.1] !text-[80px]" data-splitting>
+              Empowering <br/>
+              Independence.
+            </p>
+          </div> 
+          <div className=" w-full relative pt-14 sec-head " > 
+            <span className="tbg  ">BUILDING</span>
+              <p className=" font-medium text-lg mx-auto txt text-white lg:max-w-md " >Open web democratizing music industry participation, investible talents, community-driven ownership, better monetization, incentivized social interactions, and growth.</p>
           </div>
-          <p className=" font-medium text-center -mt-14 mx-auto text-white lg:max-w-2xl " >Open web democratizing music industry participation, investible talents, community-driven ownership, better monetization, incentivized social interactions, and growth.</p>
         </div>    
         <div className=" w-full  " > 
-          <Carousel slides={slides} autoplay={false} onSlideChange={callback}/> 
+          <Carousel slides={slides} autoplay={true} interval={4000} /> 
         </div>
         {/* <div className=" pt-8 w-full flex flex-col items-center " > 
           {tab === 0 && (
